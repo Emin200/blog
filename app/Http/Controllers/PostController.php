8bd059c9e499fa1b31acdfa6ad63fb99f1273348
emin <abdullayev.emin@mail.ru>
2020-04-27 21:40:02 +0400

    test github

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -90,7 +90,7 @@ class PostController extends Controller
     {
         $post = Post::join('users', 'author_id', '=', 'users.id')
                     ->find($id);
-        
+
         if (!$post){
             return redirect()->route('post.index')->withErrors('Пост не найден!');
         }
@@ -106,6 +106,7 @@ class PostController extends Controller
      */
     public function edit($id)
     {
+        
         $post = Post::find($id);
 
         if (!$post){
